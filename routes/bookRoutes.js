8609dd6bc20756adad9d8967b4e23361c763fcc0
AhamadShaikh <shaikhahamadrasul786@gmail.com>
2024-01-07 00:00:22 +0530

    filter functionality

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -28,9 +28,9 @@ router.get("/books", async (req, res) => {
         console.log(sortBy);
 
 
-        // if(category){
-        //     query.category = { $regex: categoryFilter, $options: "i" };
-        // }
+        if(category){
+            query.category = { $regex: categoryFilter, $options: "i" };
+        }
 
         const books = await Book.find(query)
             .sort(sortBy)
@@ -89,72 +89,3 @@ router.post('/create', middleware, async (req, res) => {
 })
 
 module.exports = router
-
-
-
-
-
-
-
-
-
-
-
-// router.get("/books", async (req, res) => {
-//     try {
-//         const page = parseInt(req.query.page) || 1;
-//         const limit = parseInt(req.query.limit) || 5;
-//         const search = req.query.search || "";
-//         const sortField = req.query.sort || "release_year";
-//         const sortOrder = req.query.order || "asc";
-//         if(ca)
-//         const categories = [req.query.category] || [];
-//         console.log(categories);
-
-//         let query = {}
-
-//         if (search || categories.length > 0) {
-//             query.$or = [
-//                 { book_name: { $regex: search, $options: "i" } },
-//                 { category: { $in: categories.map(cat => new RegExp(cat, 'i')) } },
-//                 { release_year: { $regex: search, $options: "i" } }
-//             ];
-//         }
-
-//         const sortBy = {};
-//         sortBy[sortField] = sortOrder;
-//         console.log(sortBy); 
-
-
-//         // if(category){
-//         //     query.category = { $regex: category, $options: "i" };
-//         //     query.category = { $regex: category, $options: "i" };
-//         // }
-
-//         const books = await Book.find(query)
-//             .sort(sortBy)
-//             .skip((page - 1) * limit)
-//             .limit(limit);
-
-//         const total = await Book.countDocuments({
-//             $or: [
-//                 { book_name: { $regex: search, $options: "i" } },
-//                 { category: { $regex: search, $options: "i" } },
-//                 { release_year: { $regex: search, $options: "i" } }
-//             ]
-//         });
-
-//         const response = {
-//             error: false,
-//             total,
-//             page,
-//             limit,
-//             books,
-//         };
-
-//         res.status(200).json(response);
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).json({ error: true, message: "Internal Server Error" });
-//     }
-// });
